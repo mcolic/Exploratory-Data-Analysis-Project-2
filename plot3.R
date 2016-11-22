aaf65e016810580306effbbbc575d7c3af378152
mcolic <medina.colic@gmail.com>
2016-11-22 10:35:15 -0600

    Project 2

---
+++ plot3.R
@@ -0,0 +1,17 @@
+############################################
+# Author: Medina Colic
+# Course: Exploratory Data Analysis
+# Assignment: Course Project 2
+############################################
+
+################################################################################
+# Question 2
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland
+# (𝚏𝚒𝚙𝚜 == "𝟸𝟺𝟻𝟷𝟶") from 1999 to 2008? Use the base plotting system to make a
+# plot answering this question.
+################################################################################
+
+# Reading the two files using readRDS() function
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
